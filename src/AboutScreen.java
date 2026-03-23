58e313a69d79d924c7c0e57447a9cc289e5ebd00
KhornVictor <KhornVictor@users.noreply.github.com>
2026-03-23 13:52:53 +0700

    Create User Registeration Screen

--- src/AboutScreen.java
+++
@@ -1,6 +0,0 @@
-class AboutScreen {
-    void show() {
-        System.out.println("===== ABOUT US =====");
-        System.out.println("Typing Test System for I3 midterm project.");
-    }
-}
--- src/UserStore.java
+++
@@ -1,6 +0,0 @@
-class UserStore {
-    static final UserRepository REPOSITORY = new UserRepository();
-
-    private UserStore() {
-    }
-}
